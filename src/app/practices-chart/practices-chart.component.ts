4f06029ee51ce3a2b99bc19563185241c23d55fe
Luís Soares <lsoares@gmail.com>
2019-03-15 23:25:11 +0000

    View chart by day/week/month

--- src/app/practices-chart/practices-chart.component.ts
+++ src/app/practices-chart/practices-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { MusicalPiece } from '../model/musical-piece';
 import { PracticeTimePipe } from '../practice-time.pipe';
@@ -11,9 +11,10 @@ import { PracticeTimePipe } from '../practice-time.pipe';
 export class PracticesChartComponent implements OnInit {
 
   @Input() musicalPieces: MusicalPiece[];
-  @Input() days;
   @Input() showLegend;
 
+  type = 'day';
+  columnCount = 7;
   barChartOptions: any = {
     legend: { display: false },
     scaleShowVerticalLines: false,
@@ -33,29 +34,34 @@ export class PracticesChartComponent implements OnInit {
 
   ngOnInit() {
     this.barChartOptions.legend.display = this.showLegend;
+
+    let multiplier = 1;
+    if (this.type === 'week') {
+      multiplier = 7;
+    } else if (this.type === 'month') {
+      multiplier = 30;
+    }
     // labels
-    for (let i = this.days; i > 0; i--) {
+    this.barChartLabels = [];
+    for (let i = this.columnCount; i > 0; i--) {
       const date = new Date();
-      date.setDate(date.getDate() - i + 1);
+      date.setDate(date.getDate() - i * multiplier + 1);
       this.barChartLabels.push(moment(this.atMidNight(date)).format('Do MMM'));
     }
     // data
+    this.barChartData = [];
     this.musicalPieces.forEach((musicalPiece, i) => {
       this.barChartData[i] = {};
       this.barChartData[i].label = musicalPiece.title;
 
-      this.barChartData[i].data = Array(this.days).fill(0);
+      this.barChartData[i].data = Array(this.columnCount).fill(0);
       musicalPiece.practices.forEach(practice => {
-        const dayOffset = Math.round((
+        const offset = Math.round((
           this.atMidNight(new Date()).getTime() - this.atMidNight(practice.startDate).getTime()) / (1000 * 60 * 60 * 24)
         ) * multiplier;
-        this.barChartData[i].data[this.days - offset - 1] += PracticeTimePipe.getDuration(practice);
+        this.barChartData[i].data[this.columnCount - offset - 1] += PracticeTimePipe.getDuration(practice);
       });
     });
-  }
-
-  getDuration(practice: Practice) {
-    return Math.round(((practice.endDate || new Date()).getTime() - practice.startDate.getTime()) / (60 * 1000));
   }
 
   private atMidNight(date: Date) {
